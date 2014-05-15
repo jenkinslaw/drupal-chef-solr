3b40d1fbd431e869ba6fa8b7d8fb4e2e13c5efae
David Kinzer <dtkinzer@gmail.com>
2014-05-15 14:31:23 -0400

    Add back the drupal  config-files path.

--- attributes/drupal.rb
+++ attributes/drupal.rb
@@ -6,6 +6,7 @@
 #
 default['solr']['drupal']['version']  = '6.x-3.x-dev'
 default['solr']['drupal']['url'] = "http://ftp.drupal.org/files/projects/apachesolr-#{node['solr']['drupal']['version']}.tar.gz"
-default['solr']['drupal']['config_files'] = ['schema.xml', 'solrconfig.xml', 'protwords.txt'] 
+default['solr']['drupal']['config_files'] = ['schema.xml', 'solrconfig.xml', 'protwords.txt']
 default['solr']['drupal']['config_files_path'] =
+  "apachesolr-#{node['solr']['drupal']['version']}/solr-conf/solr-4.x"
 default['solr']['drupal']['disable_stemming'] = FALSE
